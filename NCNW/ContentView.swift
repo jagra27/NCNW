690177129f84216882cc12288525671289aa92d7
Jada Grant <jagra27@morgan.edu>
2020-08-22 11:24:17 -0400

    Initial Commit

---
+++ NCNW/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  NCNW
+//
+//  Created by Jada Grant on 8/22/20.
+//  Copyright © 2020 Jada Grant. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
