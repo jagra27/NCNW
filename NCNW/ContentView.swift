c5053f014a7d3f5c9fb3cc17f252e3c8b74136ae
Jada Grant <jagra27@morgan.edu>
2020-08-22 18:01:29 -0400

    UI for login page

--- NCNW/ContentView.swift
+++ NCNW/ContentView.swift
@@ -10,12 +10,184 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        
+        Home()
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct Home : View {
+    
+    var body: some View{
+        
+        VStack{
+            
+            Login()
+        }
+    }
+}
+
+struct Login: View {
+    
+    @State var color = Color.black.opacity(0.7)
+    @State var email = ""
+    @State var pass = ""
+    @State var visible = false
+    @State var alert = false
+    @State var error = ""
+    
+    var body: some View{
+        
+        ZStack{
+                VStack{
+                    
+                    Image("ncnwlogo")
+                        .resizable()
+                        .frame(width: 380, height: 140)
+                    
+                    Text("Log in to your account")
+                        .font(.title)
+                        .fontWeight(.bold)
+                        .foregroundColor(self.color)
+                    
+                    TextField("Email", text: self.$email)
+                    .padding()
+                        .background(RoundedRectangle(cornerRadius: 4).stroke(self.email != "" ? Color("Color") : self.color, lineWidth: 2))
+                        .padding(.top, 25)
+                        .frame(width: 300, height: nil)
+                
+                    HStack(spacing: 15){
+                    
+                    VStack{
+                        
+                        if self.visible{
+                            
+                            TextField("Password", text: self.$pass)
+                        }
+                        else{
+                            
+                            SecureField("Password", text: self.$pass)
+                        }
+                    }
+                        
+                    Button(action: {
+                        
+                        self.visible.toggle()
+                        
+                    }) {
+                        Image(systemName: self.visible ? "eye.slash.fill" : "eye.fill")
+                            .foregroundColor(self.color)
+                    }
+                }
+                    .padding()
+                    .background(RoundedRectangle(cornerRadius: 4).stroke(self.pass != "" ? Color("Color") : self.color, lineWidth: 2))
+                    .padding(.top, 25)
+                    .frame(width: 300, height: nil)
+            
+                
+                HStack{
+                    
+                    Spacer()
+                    
+                    Button(action: {
+                        
+                    }) {
+                        Text("Forgot Password")
+                            .fontWeight(.bold)
+                        .foregroundColor(Color("Color"))
+                            .padding(.horizontal, 10)
+                    }
+                }
+                .padding(.top, 20)
+                    
+                    Button(action: {
+                        
+                        self.verify()
+                        
+                    }) {
+                        
+                        Text("Log in")
+                            .foregroundColor(.white)
+                            .padding(.vertical)
+                            .frame(width: UIScreen.main.bounds.width - 50)
+                    }
+                .background(Color("Color"))
+                .cornerRadius(10)
+                .padding(.top, 25)
+            }
+                .padding(.horizontal,25)
+            
+            if self.alert{
+                ErrorView(alert: self.$alert, error: self.$error)
+            }
+        }
+    }
+    
+    func verify(){
+        
+        if self.email != "" && self.pass != ""{
+            
+        }
+        else{
+            self.error = "Please fill in all the fields"
+            self.alert.toggle()
+        }
+    }
+}
+
+struct ErrorView : View{
+    
+    @State var color = Color.black.opacity(0.7)
+    @Binding var alert : Bool
+    @Binding var error : String
+    
+    var body : some View{
+        
+        GeometryReader{_ in
+            
+            VStack{
+                
+                HStack{
+                    
+                    Text("Error")
+                        .font(.title)
+                        .fontWeight(.bold)
+                        .foregroundColor(self.color)
+                    
+                    Spacer()
+                    
+                }
+                .padding(.horizontal, 25)
+                
+                Text(self.error)
+                    .foregroundColor(self.color)
+                    .padding(.top)
+                    .padding(.horizontal, 25)
+                
+                Button(action: {
+                    
+                    self.alert.toggle()
+                    
+                }) {
+                    Text("Cancel")
+                        .foregroundColor(.white)
+                        .padding(.vertical)
+                        .frame(width: UIScreen.main.bounds.width - 120)
+                }
+                .background(Color("Color"))
+                .cornerRadius(10)
+                .padding(.top, 25)
+            }
+            .padding(.vertical, 25)
+            .frame(width: UIScreen.main.bounds.width - 70)
+            .background(Color.white)
+            .cornerRadius(15)
+        }
+        .background(Color.black.opacity(0.35).edgesIgnoringSafeArea(.all))
     }
 }
